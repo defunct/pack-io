67d451b9f205e73906fb03fe46671d0356b48bc7
Alan Gutierrez <alan@blogometer.com>
2009-12-05 05:42:44 -0600

    Mix project.

---
+++ src/mix/java/com/goodworkalan/pack/io/PackIOProject.java
@@ -0,0 +1,27 @@
+package com.goodworkalan.pack.io.mix;
+
+import com.goodworkalan.go.go.Artifact;
+import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Builder;
+import com.goodworkalan.mix.builder.JavaProject;
+
+public class PackIOProject extends ProjectModule {
+    @Override
+    public void build(Builder builder) {
+        builder
+            .cookbook(JavaProject.class)
+                .produces(new Artifact("com.goodworkalan/pack-io/0.1"))
+                .main()
+                    .depends()
+                        .artifact(new Artifact("com.goodworkalan/pack/0.1"))
+                        .end()
+                    .end()
+                .test()
+                    .depends()
+                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .end()
+                    .end()
+                .end()
+            .end();
+    }
+}
