6016652d165d1886cbe7e7655897a885496ad4f0
Alan Gutierrez <alan@blogometer.com>
2010-06-03 01:12:14 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/pack/io/PackIOProject.java
+++ src/mix/java/com/goodworkalan/pack/io/PackIOProject.java
@@ -4,21 +4,25 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class PackIOProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Pack I/O.
+ *
+ * @author Alan Gutierrez
+ */
+public class PackIOProject implements ProjectModule {
+    /**
+     * Build the project definition for Pack I/O.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.pack/pack-io/0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.pack/pack/0.+1")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.pack/pack/0.+1")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
